38c96e5ac43793647a6c7783fc0112857bd515ca
FlorianFaust <florian.faust@simovative.com>
2023-10-31 09:40:24 +0100

    123456789

--- public/calc.php
+++ public/calc.php
@@ -1,6 +1,6 @@
 <?php
 
-use Simovative\Learn\Calculator;
+use Simovative\Learn\Check;
 
 spl_autoload_register(function ($className)
 {
@@ -9,42 +9,7 @@ spl_autoload_register(function ($className)
     include '../src/' . $className . '.php';
 });
 
-
-function hasOneOperator (string $input): string {
-    $operatorArray = ['+', '-', '*', '/'];
-    foreach ($operatorArray as $operator) {
-        if (str_contains($input, $operator)) {
-            return $operator;
-        }
-    }
-    return false;
-}
-
-
-function exploder (string $input, string $operator) {
-    return explode($operator, $input);
-};
-
 $result = $_REQUEST['input'] ?? '';
+$check = new Check();
+$check->check($result);
 
-function check () {
-
-}
-
-if (!hasOneOperator($result)) {
-    $error = 'Passt nicht';
-    require 'main.php';
-    exit();
-} else {
-    $operator = hasOneOperator($result);
-    $stringArray = exploder($result, $operator);
-    $calc = new Calculator((float)$stringArray[0], (float) $stringArray[1], $operator);
-    $calc->calculate();
-    $result = $calc->getResult();
-    require 'main.php';
-
-
-
-}
-
-//composer einbinden, composer.json, docker exec, composer phar, ..., requiere installieren, PSR4 Autoload
---
+++ src/CheckInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Simovative\Learn;
+
+interface CheckInterface
+{
+    public function check ($result) : void;
+
+}
